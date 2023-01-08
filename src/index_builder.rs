de74da599b975fbe4fce63cdbc4c223ff9259266
Volodymyr Tesliuk <vovatesluk6a@gmail.com>
2023-01-08 04:41:03 +0200

    (refactor) use builder pattern to build index

---
+++ src/index_builder.rs
@@ -0,0 +1,54 @@
+use std::{collections::{HashMap, HashSet}, sync::{Mutex, Arc}, fs::{File}, io::{BufReader, Read}, thread, time::Duration, path::{Path, PathBuf}};
+use bimap::BiMap;
+use threadpool::ThreadPool;
+
+pub struct IndexBuilder {
+    cur_id: usize,
+    file_ids: BiMap::<PathBuf, usize>,
+    inverted_index: Arc<Mutex<HashMap<String, HashSet<usize>>>>,
+    thread_pool: ThreadPool,
+}
+
+impl IndexBuilder {
+    pub fn new(thread_num: usize) -> IndexBuilder {
+        IndexBuilder {
+            cur_id: 0,
+            file_ids: BiMap::new(),
+            inverted_index: Arc::new(Mutex::new(HashMap::new())),
+            thread_pool: ThreadPool::new(thread_num),
+        }
+    }
+
+    pub fn proceed(&mut self, filename: PathBuf) {
+        let file_id = self.cur_id;
+        self.file_ids.insert(filename.clone(), file_id);
+        self.cur_id += 1;
+        let inverted_index = self.inverted_index.clone();
+        self.thread_pool.execute(move ||{
+            let mut file_content = String::new();
+            let file = File::open(filename.as_path()).expect("no such file");
+            let mut buf = BufReader::new(file);
+            buf.read_to_string(&mut file_content);
+
+            let words: HashSet<&str>  = file_content.split(&[' ', '\n'][..]).collect();
+            for word in &words {
+                let mut guard = inverted_index.lock().unwrap();
+                guard
+                    .entry(word.to_string())
+                    .and_modify(|set| { set.insert(file_id); })
+                    .or_insert(HashSet::from_iter(vec![file_id]));
+            }
+        });
+    }
+
+    pub fn build(self) -> HashMap<String, HashSet<usize>>{
+        self.thread_pool.join();
+
+        let index = match Arc::try_unwrap(self.inverted_index) {
+            Ok(index) => index,
+            Err(_) => panic!(),
+        };
+
+        index.into_inner().unwrap()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,10 @@
-use std::{collections::{HashMap, HashSet}, sync::{Mutex, Arc}, fs::{File, ReadDir}, io::{BufReader, Read}, thread, time::Duration, path::{Path, PathBuf}};
-use bimap::BiMap;
 use clap::Parser;
-use threadpool::ThreadPool;
 use std::fs;
 
 mod cli;
 use cli::Cli;
-
-fn build_index<'a>(filenames: impl Iterator<Item = PathBuf>) {
-    let mut inversed_index =
-        Arc::new(Mutex::new(HashMap::<String, HashSet<usize>>::new()));
-    let mut file_ids = BiMap::<PathBuf, usize>::new();
-    let thread_pool = ThreadPool::new(8);
-    let mut cur_id: usize = 0;
-
-    for file in filenames {
-        let file = file.to_path_buf();
-        let inversed_index = inversed_index.clone();
-        file_ids.insert(file.clone(), cur_id);
-
-        thread_pool.execute(move ||{
-            let file_id = cur_id;
-            let mut file_content = String::new();
-            let file = File::open(file).expect("no such file");
-            let mut buf = BufReader::new(file);
-            buf.read_to_string(&mut file_content);
-
-            let words: HashSet<&str>  = file_content.split(&[' ', '\n'][..]).collect();
-            for word in &words {
-                let mut guard = inversed_index.lock().unwrap();
-                guard
-                    .entry(word.to_string())
-                    .and_modify(|set| { set.insert(file_id); })
-                    .or_insert(HashSet::from_iter(vec![file_id]));
-            }
-
-        });
-        cur_id += 1;
-    }
-    thread_pool.join();
-    println!("{:?}", inversed_index);
-}
+mod index_builder;
+use crate::index_builder::IndexBuilder;
 
 fn main() {
     let cli = Cli::parse();
@@ -53,6 +17,11 @@ fn main() {
     };
 
     println!("{:?} {} {}", cli.files_dir, cli.build_index, cli.start_server);
-    //let filenames = vec!["filename1.txt"];
-    build_index(filenames);
+
+    let mut builder = IndexBuilder::new(8);
+    for file in filenames {
+        builder.proceed(file);
+    }
+    let index = builder.build();
+    println!("{:?}", index);
 }
