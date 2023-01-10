caad479f17d3016ce14ecc6608d00719b8f7e5b2
Vovanoxin <vovatesluk6a@gmail.com>
2023-01-10 03:05:25 +0200

    (merge) implement basic functionality
    * (feat) basic thread pool implementation
    
    * (refactor) refactor threadpool
    
    * (refactor) replace threadpool with crate
    
    * (feat) implement basic inverted index building
    
    * (feat) implement CLI arguments handling
    
    * (refactor) use builder pattern to build index
    
    * (feat) make possible recursive directory processing
    
    * (feat) serialize-deserialize index structure
    
    * (feat) specify threads number in CLI argument

---
+++ src/cli.rs
@@ -0,0 +1,28 @@
+use std::path::PathBuf;
+
+use clap::{Parser, ArgGroup};
+
+// --build_index --files_dir --index_path --start_server
+// якщо вказано і --build_index можна щоб --index_path була відсутня
+// - тоді структура створюється в памʼяті.
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(group(
+    ArgGroup::new("mode").required(true).multiple(true).args(["build_index", "start_server"])))]
+pub struct Cli {
+    #[arg(long, required = true)]
+    pub files_dir: PathBuf,
+
+    #[arg(long, required_unless_present="build_index")]
+    pub index_path: Option<PathBuf>,
+
+    #[arg(long, default_value_t = false)]
+    pub build_index: bool,
+
+    #[arg(long, default_value_t = false)]
+    pub start_server: bool,
+
+    #[arg(long, default_value_t = 1)]
+    pub thread_number: usize,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+extern crate serde;
+extern crate serde_json;
+
+use clap::Parser;
+use walkdir::WalkDir;
+
+mod cli;
+use cli::Cli;
+mod index_builder;
+use crate::index_builder::IndexBuilder;
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    println!("{:?} {} {}", cli.files_dir, cli.build_index, cli.start_server);
+    let index =
+        if cli.build_index {
+            let mut builder = IndexBuilder::new(cli.thread_number);
+
+            for entry in WalkDir::new(cli.files_dir) {
+                let entry = entry.unwrap();
+                let path = entry.path();
+
+                if path.is_file() {
+                    builder.proceed(path.to_path_buf());
+                }
+            }
+            let index = builder.build();
+            if cli.index_path.is_some() {
+                let serialized = serde_json::to_string(&index).unwrap();
+                std::fs::write(cli.index_path.unwrap(), serialized).unwrap();
+            }
+            index
+        } else {
+            let serialized =
+                std::fs::read_to_string(cli.index_path.unwrap()).unwrap();
+            serde_json::from_str(&serialized).unwrap()
+        };
 }
